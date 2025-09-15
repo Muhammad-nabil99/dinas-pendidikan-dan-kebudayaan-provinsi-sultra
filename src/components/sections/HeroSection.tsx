b2d994f25cc3efab530bc012cb8b20f47a8cd2e4
aziscout <abdulaziscout@gmail.com>
2025-09-15 23:10:01 +0800

    update image  hero-section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,29 +1,45 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRight, GraduationCap } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import heroImage1 from "@/assets/hero-1.jpg";
 import heroImage2 from "@/assets/hero-2.png";
 import heroImage3 from "@/assets/hero-3.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const heroImage = [heroImage1, heroImage2, heroImage3];
 
 const HeroSection = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Ganti gambar setiap 5 detik
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % heroImage.length);
+    }, 15000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section
       id="hero"
-      className="relative h-screen flex items-center overflow-hidden "
+      className="relative h-screen flex items-center overflow-hidden"
     >
-      {/* Background Image */}
-      <div className="absolute inset-0 z-0 ">
-        <img
-          src={heroImage1}
-          alt="Dinas Pendidikan Sulawesi Tenggara"
-          className="w-full h-full object-cover"
-        />
+      {/* Background Slideshow */}
+      <div className="absolute inset-0 z-0">
+        {heroImage.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index}`}
+            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        ))}
         <div className="absolute inset-0 bg-gradient-to-r from-government-blue/90 via-government-blue/70 to-transparent"></div>
       </div>
+
       {/* Content */}
-      {/* <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20"> */}
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32 pt-60 md:pt-20 lg:pt-40">
         <div className="max-w-3xl text-center mx-auto">
           <div className="flex items-center justify-center space-x-3 mb-6">
@@ -108,3 +124,5 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
+      
