d95a5d341cd2c12a1ed8fa88f49008ad4b9d076c
Muhammad-nabil99 <muhammad.nabil290303@gmail.com>
2025-09-20 17:43:43 +0800

    add animation to text on hero section

--- src/pages/laporanKegiatan/LaporanKegiatan.tsx
+++ src/pages/laporanKegiatan/LaporanKegiatan.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
+
 const LaporanKegiatan = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedYear, setSelectedYear] = useState("2024");
